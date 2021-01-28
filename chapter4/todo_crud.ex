b9ecc0f056682e3d2352b8e11b7cac6406a10779
ForrestFairy <szymon.bogusz55@gmail.com>
2021-01-28 20:31:13 +0000

    Added update and delete functions to todo list

--- chapter4/todo_crud.ex
+++ chapter4/todo_crud.ex
@@ -17,4 +17,36 @@ defmodule TodoList do
     }
   end
 
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+    |> Enum.map(fn({_, entry}) -> entry end)
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+    ) do
+      case entries[entry_id] of
+        nil -> todo_list
+
+        old_entry ->
+          new_entry = updater_fun(old_entry)
+          new_entries = HashDict.put(entries, new_entry.id, new_entry)
+          %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+  ) do
+    %TodoLsit{todo_list | HashDict.delete(todo_list, entry_id)}
+  end
+
 end
