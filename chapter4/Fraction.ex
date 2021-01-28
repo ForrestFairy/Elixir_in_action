718231f6152dab2f48df2c6ecd2e0e4b2754ff49
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-01-28 18:08:46 +0100

    Made another todoList but this time with a struct

--- chapter4/Fraction.ex
+++ chapter4/Fraction.ex
@@ -8,4 +8,16 @@ defmodule Fraction do
 
   def value(%Fraction{a: a, b: b}), do: a/b
 
+  def value(fraction) do
+    fraction.a / fraction.b
+  end
+
+  def add(
+    %Fraction{a: a1, b: b1},
+    %Fraction{a: a2, b: b2}
+  ) do
+    new((a1 * b2) + (a2 * b1),
+        (b1 * b2)
+        )
+  end
 end
