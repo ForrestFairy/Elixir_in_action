938e37590b68bd8dad67d89f50836bf9bde52500
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-03-31 11:11:39 +0200

    http serve kinda works, it was made with cowboy framework

---
+++ chapter11/todo_web/lib/todo/web.ex
@@ -0,0 +1,37 @@
+defmodule Todo.Web do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  def start_server do
+    Plug.Adapters.Cowboy.http(__MODULE__, nil, port: 5454)
+  end
+
+  post "/add_entry" do
+    conn
+    |> Plug.Conn.fetch_params
+    |> add_entry
+    |> respond
+  end
+
+  def add_entry(conn) do
+    conn.params["list"]
+    |> Todo.Cache.server_process
+    |> Todo.Server.add_entry(
+      %{
+        date: Date.from_iso8601!(conn.params["date"]),
+        title: conn.params["title"]
+      }
+    )
+
+    Plug.Conn.asign(conn, :response, "OK")
+  end
+
+  def respond(conn) do
+    conn
+    |> Plug.Conn.put_resp_content_type("tex/plain")
+    |> Plug.Conn.send_resp(200, conn.asigns[:response])
+  end
+
+end
