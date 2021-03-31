797d926acb4398b979113717f813bbc308aba2d3
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-03-31 12:08:16 +0200

    Chapter 11 is done with exploring how to set up environment variables

---
+++ chapter11/todo_env/mix.exs
@@ -0,0 +1,37 @@
+defmodule TodoWeb.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :todo_web,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      applications: [:logger, :gproc, :cowboy, :plug],
+      mod: {Todo.Application, []},
+      env: [
+        port: 5454
+      ]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:gproc, "0.3.1"},
+      {:cowboy, "1.1.0"},
+      {:plug, "1.4.0"},
+      {:meck, "0.8.2", only: :test},
+      {:httpoison, "0.4.3", only: :test}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
