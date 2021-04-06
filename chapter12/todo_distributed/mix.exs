dfc2a37e808ff644b490502091ce9342468275dd
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-04-06 10:48:55 +0200

    Chapter 12 done, it made the todo app from chapter 11 distributed But it still doesn't wok because something is wrong with plug

---
+++ chapter12/todo_distributed/mix.exs
@@ -0,0 +1,32 @@
+defmodule GprocTodoApp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gproc_todo_app,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      applications: [:logger, :gproc, :cowboy, :plug],
+      mod: {Todo.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:gproc, "0.3.1"},
+      {:cowboy, "1.0.0"},
+      {:plug, "0.10.0"},
+      {:meck, "0.8.2", only: :test},
+      {:httpoison, "0.4.3", only: :test}
+    ]
+  end
+end
